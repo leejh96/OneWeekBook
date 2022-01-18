8a4c099adbc96682c7961c9ddbd142bb4524892a
leejh96 <dlwngur96@naver.com>
2022-01-18 22:57:25 +0900

    이메일인증 타이머구현

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,11 +1,24 @@
 const { sendEmail } = require("../modules/nodeMailerModule");
 let codeNumber = -111111;
+let ing = false;
+let timer = null;
+
 const authController = {
   sendCode: (req, res) => {
+    if (ing) {
+      return res.status(400).json({
+        success: false,
+        message: "이미 이메일이 전송되었습니다.",
+      });
+    }
     const { email } = req.body;
     codeNumber = Math.floor(Math.random() * 1000000);
     const boolSendEmail = sendEmail(email, codeNumber);
     if (boolSendEmail) {
+      ing = true;
+      timer = setTimeout(() => {
+        ing = false;
+      }, 180000);
       return res.status(200).json({
         message: "이메일 전송 완료!",
         success: true,
@@ -18,8 +31,16 @@ const authController = {
   },
 
   authEmail: (req, res) => {
+    if (!ing) {
+      return res.status(408).json({
+        success: false,
+        message: "이메인 인증 제한시간이 만료되었습니다.",
+      });
+    }
     const { code } = req.body;
     if (parseInt(code) === codeNumber) {
+      ing = false;
+      clearTimeout(timer);
       res.status(200).json({
         message: "이메일 인증에 성공했습니다.",
         success: true,
